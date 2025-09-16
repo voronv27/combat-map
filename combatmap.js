558aa75dccc286123becab6186926270e35a5850
voronv27 <152320775+voronv27@users.noreply.github.com>
2025-09-16 13:41:56 -0400

    Add image upload and bootstrap
    Added in a button to upload an image and display on the site, with the goal of making this the "background" of the combat map later down the line. Also added bootstrap for easier styling.

---
+++ combatmap.js
@@ -0,0 +1,19 @@
+// Add a listener for changes in the combat map image
+window.addEventListener( 'load', function() {
+    document.getElementById( 'fileInput' ).addEventListener( 'change', function() {
+        // Remove previous image
+        var mapBg = document.getElementById( 'mapBg' );
+        mapBg.onload = () => {
+            if ( mapBg.src ) {
+                URL.revokeObjectURL( mapBg.src );  // no longer needed, free memory
+            }
+        }
+        if ( !this.files || !this.files[0] ) {
+            return;
+        }
+
+        // Display new image
+        var img = this.files[0];
+        mapBg.src = URL.createObjectURL( img ); // create a blob url for the image
+    });
+});
