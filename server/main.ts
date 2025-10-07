54c0adb8bbfbe8fd049026172c105fdb1715bedb
voronv27 <152320775+voronv27@users.noreply.github.com>
2025-10-07 15:21:13 -0400

    Blame Deno Deploy for making me do this

--- server/main.ts
+++ server/main.ts
@@ -20,9 +20,8 @@ async function handler(req: Request): Promise<Reponse> {
   const url = new URL(req.url);
   console.log(url);
   console.log("Request headers:");
-  console.log(req.headers);
-  if (req.headers.get("upgrade") === "websocket" ||
-      url.pathname === "/start_web_socket") {
+  console.log(`${req.headers}`);
+  if (req.headers.get("upgrade") === "websocket") {
     console.log("upgrade to websocket");
     return server.handleConnection(req);
   }
